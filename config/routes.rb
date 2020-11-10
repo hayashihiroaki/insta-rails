3d89153d511b08ee855bb1e69209885ed4b0d364
hayashihiroaki <61njn0@gmail.com>
2020-11-10 09:56:29 +0900

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "home#index"
+
+  resources :photos
+  resources :users do
+    resource :follow
+    resources :followings
+    resources :followers
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
